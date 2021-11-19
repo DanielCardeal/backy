5ce1ed599f5e66a390b142dd8d9f83477d42bfa1
Daniel Pessoa Cardeal <cardeal.daniel@usp.br>
2021-11-19 12:12:29 -0300

    Encapsula a execução do programa na função run_app

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,19 @@ mod commands;
 mod error;
 
 use commands::Command;
+use error::BackyError;
 use std::{env, process::exit};
 
-fn main() {
+fn run_app() -> Result<(), BackyError> {
     let args: Vec<String> = env::args().collect();
-    let command = Command::from_args(&args).unwrap_or_else(|err| {
+    let command = Command::from_args(&args)?;
+    command.execute();
+    Ok(())
+}
+
+fn main() {
+    if let Err(err) = run_app() {
         eprintln!("{}", err);
         exit(1);
-    });
-    command.execute();
+    }
 }
