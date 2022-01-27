614fb0deecf852b7085226b29bef2edc79c0a081
Daniel Pessoa Cardeal <cardeal.daniel@usp.br>
2022-01-27 00:12:08 -0300

    Cria trait BackyError e converte erros para structs.
    Nessa nova implementação, a proposta é usar o poder dos traits para
    simplificar o código e permitir a criação de erros de maneira
    decentralizada. Essa nova abordagem tenta facilitar a legibilidade e
    extensibilidade do código.

--- src/error.rs
+++ src/error.rs
@@ -1,67 +1,97 @@
-use core::fmt;
 use std::io;
 
-/// Erros que podem ocorrer durante a execução do programa
-pub enum BackyError {
-    /// Não foi passado um comando par o programa
-    NoCommand,
-    /// O comando passado não existe
-    BadCommand(String),
-
-    /// Não foi possível abrir o arquivo de configuração
-    NoConfig,
-    /// Não foi possível encontrar o diretório de configuração
-    NoConfigDir,
-    /// O arquivo de configuração contém erros de formatação ou typos
-    BadConfigFormat(toml::de::Error),
-
-    /// Não foi possível criar o diretório de backup
-    NoArchiveDir(io::Error),
-    /// Alguns dos arquivos de backup não existem no sistema
-    BadFiles(Vec<String>),
-    /// Não foi possível criar o link simbólico nos archives
-    SymCreationFailed(io::Error),
-
-    /// Não foi possível encontrar o comando rsync no PATH do usuário
-    NoRsync,
+/// Um erro encontrado durante a execução do programa.
+pub trait BackyError {
+    fn display(&self) {
+        let msg = self.get_err_msg();
+        eprintln!("Error: {}", msg);
+    }
+    fn get_err_msg(&self) -> &str;
 }
 
-impl BackyError {
-    fn get_err_msg(&self) -> String {
-        match self {
-            BackyError::NoCommand => {
-                "no command to execute. Try `backy help` for aditional information.".to_string()
-            }
-            BackyError::BadCommand(cmd) => format!("command '{}' doesn't exist", cmd),
-            BackyError::NoConfig => {
-                "unable to open the configuration file (maybe the file doesn't exist?)".to_string()
-            }
-            BackyError::NoConfigDir => "unable to open the configuration directory".to_string(),
-            BackyError::BadConfigFormat(err) => {
-                format!("unable to parse config:\n{}", err).to_string()
-            }
-            BackyError::NoArchiveDir(err) => {
-                format!("unable to create backup dir:\n{}", err)
-            }
-            BackyError::BadFiles(files) => {
-                let mut msg = String::from("unable to find the following files:");
-                for file in files {
-                    msg.push('\n');
-                    msg.push_str(file);
-                }
-                msg
-            }
-            BackyError::SymCreationFailed(err) => {
-                format!("unable to create `latest` symlink:\n{}", err)
-            }
-            BackyError::NoRsync => "unable to find `rsync` executable".to_string(),
-        }
+/// Não foi passado um comando par o programa
+struct ErrNoCommand;
+impl BackyError for ErrNoCommand {
+    fn get_err_msg(&self) -> &str {
+        "no command to execute. Try `backy help` for aditional information."
     }
 }
 
-impl fmt::Display for BackyError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let msg = self.get_err_msg();
-        write!(f, "Error: {}.", msg)
+/// O comando passado não existe
+struct ErrBadCommand {
+    cmd: String,
+}
+impl BackyError for ErrBadCommand {
+    fn get_err_msg(&self) -> &str {
+        format!("command '{}' doesn't exist", self.cmd).as_str()
+    }
+}
+
+/// Não foi possível abrir o arquivo de configuração
+struct ErrNoConfigFile;
+impl BackyError for ErrNoConfigFile {
+    fn get_err_msg(&self) -> &str {
+        "unable to open the configuration file (maybe the file doesn't exist?)"
+    }
+}
+
+/// Não foi possível encontrar o diretório de configuração
+struct ErrNoConfigDir;
+impl BackyError for ErrNoConfigDir {
+    fn get_err_msg(&self) -> &str {
+        "unable to open the configuration directory"
+    }
+}
+
+/// O arquivo de configuração contém erros de formatação ou typos
+struct ErrBadConfigFormat {
+    err: toml::de::Error,
+}
+impl BackyError for ErrBadConfigFormat {
+    fn get_err_msg(&self) -> &str {
+        format!("unable to parse config:\n{}", self.err).as_str()
+    }
+}
+
+/// Não foi possível criar o diretório de backup
+struct ErrNoArchiveDir {
+    err: io::Error,
+}
+impl BackyError for ErrNoArchiveDir {
+    fn get_err_msg(&self) -> &str {
+        format!("unable to create backup dir:\n{}", self.err).as_str()
+    }
+}
+
+/// Alguns dos arquivos de backup não existem no sistema
+struct ErrBadFiles {
+    missing_files: Vec<String>,
+}
+impl BackyError for ErrBadFiles {
+    fn get_err_msg(&self) -> &str {
+        let mut msg = String::from("unable to find the following files:");
+        for file in self.missing_files {
+            msg.push('\n');
+            msg.push_str(&file);
+        }
+        msg.as_str()
+    }
+}
+
+/// Não foi possível criar o link simbólico nos archives
+struct ErrSymCreationFailed {
+    err: io::Error,
+}
+impl BackyError for ErrSymCreationFailed {
+    fn get_err_msg(&self) -> &str {
+        format!("unable to create `latest` symlink:\n{}", self.err).as_str()
+    }
+}
+
+/// Não foi possível encontrar o comando rsync no PATH do usuário
+struct ErrNoRsync;
+impl BackyError for ErrNoRsync {
+    fn get_err_msg(&self) -> &str {
+        "unable to find `rsync` executable"
     }
 }
