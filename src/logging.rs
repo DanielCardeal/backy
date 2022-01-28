a8d51c9fc749d168ac73810cbeff2fc5eef14bd2
Daniel Pessoa Cardeal <cardeal.daniel@usp.br>
2022-01-27 22:40:00 -0300

    Cria módulo de logging usando macros.

---
+++ src/logging.rs
@@ -0,0 +1,43 @@
+// #######################
+//   Definições públicas
+// #######################
+macro_rules! log {
+    (level $lvl:expr, color $col:ident, $msg:expr) => {
+        {
+            use colored::Colorize as _;
+            println!("[{}] {}", $lvl.$col(), $msg);
+        }
+    };
+
+    (level $lvl:expr, color $col:ident, $($arg:expr),*) => {
+        {
+            use colored::Colorize as _;
+            let msg = format!($($arg),*);
+            println!("[{}] {}", $lvl.$col(), msg);
+        }
+    };
+}
+
+macro_rules! info {
+    ( $msg:literal ) => {
+        log!(level "INFO", color blue, $msg);
+    };
+
+    ( $($arg:expr),* ) => {
+        log!(level "INFO", color blue, $($arg),*);
+    };
+}
+
+macro_rules! error {
+    ( $msg:literal ) => {
+        log!(level "ERROR", color red, $msg);
+    };
+
+    ( $($arg:expr),* ) => {
+        log!(level "ERROR", color red, $($arg),*);
+    };
+}
+
+pub(crate) use log;
+pub(crate) use info;
+pub(crate) use error;
